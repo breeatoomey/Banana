eb82c144f4b110d34401dc2d6ab5469dc2d1c283
DenaliTonn <dtonn1@lion.lmu.edu>
2024-02-19 18:48:36 -0800

    Implemented syntax check from command line

--- src/banana.js
+++ src/banana.js
@@ -1,3 +1,19 @@
+import * as fs from "node:fs/promises";
+import process from "node:process";
+import parse from "./parser.js";
+
 export function compile() {
-    return "eventually this will return compiled code";
+  return "eventually this will return compiled code";
+}
+
+if (process.argv.length !== 3) {
+  console.log("Must have exactly one argument: the filename of the program to compile.");
+} else {
+  try {
+    const buffer = await fs.readFile(process.argv[2]);
+    parse(buffer.toString());
+    console.log("Syntax ok");
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`);
+  }
 }
