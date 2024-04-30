8e2bbd28bf253f5749532c5c5eeaa5cec059b128
carolinelmu1 <caroline.lmu@gmail.com>
2024-04-30 02:52:12 -0700

    generator test progress and updates to analyzer/optimizer

--- src/analyzer.js
+++ src/analyzer.js
@@ -251,224 +251,256 @@ export default function analyze(match) {
   // recursion.
   const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
     Program(statements) {
-      return core.program(statements.children.map(s => s.rep()))
+      return core.program(statements.children.map((s) => s.rep()));
     },
 
-    FunDecl(_func, id, _left_paren, parameters, _right_paren, _arrow, type, block) {
+    FunDecl(
+      _func,
+      id,
+      _left_paren,
+      parameters,
+      _right_paren,
+      _arrow,
+      type,
+      block
+    ) {
       // Start by making the function, but we don't yet know its type.
       // Also add it to the context so that we can have recursion.
-      const func = core.func(id.sourceString)
-      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
-      context.add(id.sourceString, func)
+      const func = core.func(id.sourceString);
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
+      context.add(id.sourceString, func);
 
       // Parameters are part of the child context
-      context = context.newChildContext({ inLoop: false, function: func })
-      const params = parameters.rep()
-
+      context = context.newChildContext({ inLoop: false, function: func });
+      const params = parameters.rep();
+      
       // Now that the parameters are known, we compute the function's type.
       // This is fine; we did not need the type to analyze the parameters,
       // but we do need to set it before analyzing the body.
-      const paramTypes = params.map(param => param.type)
-      const returnType = type.children?.[0]?.rep() ?? VOID
-      const paramCount = params.length
-      func.type = core.functionType(paramTypes, returnType, paramCount)
+      const paramTypes = params.map((param) => param.type);
+      const returnType = type.children?.[0]?.rep() ?? VOID;
+      const paramCount = params.length;
+      func.type = core.functionType(paramTypes, returnType, paramCount);
 
       // Analyze body while still in child context
-      const body = block.rep()
+      const body = block.rep();
 
       // Go back up to the outer context before returning
-      context = context.parent
-      return core.functionDeclaration(func, params, body)
+      context = context.parent;
+      return core.functionDeclaration(func, params, body);
     },
 
     Params(param_list) {
       // Returns a list of variable nodes
-      return param_list.asIteration().children.map(p => p.rep())
+      return param_list.asIteration().children.map((p) => p.rep());
     },
 
     Param(type, id, _eq, _exp) {
-      const param = core.variable(id.sourceString, type.rep())
-      mustNotAlreadyBeDeclared(param.name, { at: id })
-      context.add(param.name, param)
-      return param
+      const param = core.variable(id.sourceString, type.rep());
+      mustNotAlreadyBeDeclared(param.name, { at: id });
+      context.add(param.name, param);
+      return param;
     },
 
     VarDecl(_modifer, _type, id, _eq, exp) {
-      const initializer = exp.rep()
-      const variable = core.variable(id.sourceString, initializer.type)
-      mustNotAlreadyBeDeclared(id.sourceString, { at: id })
-      context.add(id.sourceString, variable)
-      return core.variableDeclaration(variable, initializer)
+      const initializer = exp.rep();
+      const variable = core.variable(id.sourceString, initializer.type);
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
+      context.add(id.sourceString, variable);
+      return core.variableDeclaration(variable, initializer);
     },
 
     Assignment(variable, _eq, expression) {
-      const source = expression.rep()
-      const target = variable.rep()
-      mustBeAssignable(source, { toType: target.type }, { at: variable })
-      mustNotBeReadOnly(target, { at: variable })
-      return core.assignment(target, source)
+      const source = expression.rep();
+      const target = variable.rep();
+      mustBeAssignable(source, { toType: target.type }, { at: variable });
+      mustNotBeReadOnly(target, { at: variable });
+      return core.assignment(target, source);
     },
 
     Body(_open, statements, _close) {
       // No need for a block node, just return the list of statements
-      return statements.children.map(s => s.rep())
+      return statements.children.map((s) => s.rep());
     },
 
     ReturnStmt(return_keyword, exp) {
-      mustBeInAFunction({ at: return_keyword })
+      mustBeInAFunction({ at: return_keyword });
       // mustReturnSomething(context.function, { at: return_keyword })
-      const return_expression = exp.rep()
-      mustBeReturnable(return_expression, { from: context.function }, { at: exp })
-      return core.returnStatement(return_expression)
+      const return_expression = exp.rep();
+      mustBeReturnable(
+        return_expression,
+        { from: context.function },
+        { at: exp }
+      );
+      return core.returnStatement(return_expression);
     },
 
     Stmt_call(call) {
-      return call.rep()
+      return call.rep();
     },
-    
+
     ForStmt_collection(_for, id, _in, exp, block) {
-      const collection = exp.rep()
-      mustHaveAnArrayType(collection, { at: exp })
-      const iterator = core.variable(id.sourceString, collection.type.base_type)
-      context = context.newChildContext({ inLoop: true })
-      context.add(iterator.name, iterator)
-      const body = block.rep()  
-      context = context.parent
-      return core.forStatement(iterator, collection, body)
+      const collection = exp.rep();
+      mustHaveAnArrayType(collection, { at: exp });
+      const iterator = core.variable(
+        id.sourceString,
+        collection.type.base_type
+      );
+      context = context.newChildContext({ inLoop: true });
+      context.add(iterator.name, iterator);
+      const body = block.rep();
+      context = context.parent;
+      return core.forStatement(iterator, collection, body);
     },
 
     WhileStmt(_while, exp, block) {
-      const test = exp.rep()
-      mustHaveBooleanType(test, { at: exp })
-      context = context.newChildContext({ inLoop: true })
-      const body = block.rep()
-      context = context.parent
-      return core.whileStatement(test, body)
+      const test = exp.rep();
+      mustHaveBooleanType(test, { at: exp });
+      context = context.newChildContext({ inLoop: true });
+      const body = block.rep();
+      context = context.parent;
+      return core.whileStatement(test, body);
     },
 
     IfStmt_short(_if, exp, block) {
-      const test = exp.rep()
-      mustHaveBooleanType(test, { at: exp })
-      context = context.newChildContext()
-      const consequent = block.rep()
-      context = context.parent
-      return core.shortIfStatement(test, consequent)
+      const test = exp.rep();
+      mustHaveBooleanType(test, { at: exp });
+      context = context.newChildContext();
+      const consequent = block.rep();
+      context = context.parent;
+      return core.shortIfStatement(test, consequent);
     },
 
     IfStmt_long(_if, exp, block1, _else, block2) {
-      const test = exp.rep()
-      mustHaveBooleanType(test, { at: exp })
-      context = context.newChildContext()
-      const consequent = block1.rep()
-      context = context.parent
-      context = context.newChildContext()
-      const alternate = block2.rep()
-      context = context.parent
-      return core.longIfStatement(test, consequent, alternate)
+      const test = exp.rep();
+      mustHaveBooleanType(test, { at: exp });
+      context = context.newChildContext();
+      const consequent = block1.rep();
+      context = context.parent;
+      context = context.newChildContext();
+      const alternate = block2.rep();
+      context = context.parent;
+      return core.longIfStatement(test, consequent, alternate);
     },
 
     IfStmt_elsif(_if, exp, block, _else, trailingIfStatement) {
-      const test = exp.rep()
-      mustHaveBooleanType(test, { at: exp })
-      context = context.newChildContext()
-      const consequent = block.rep()
-      context = context.parent
-      const alternate = trailingIfStatement.rep()
-      return core.longIfStatement(test, consequent, alternate)
+      const test = exp.rep();
+      mustHaveBooleanType(test, { at: exp });
+      context = context.newChildContext();
+      const consequent = block.rep();
+      context = context.parent;
+      const alternate = trailingIfStatement.rep();
+      return core.longIfStatement(test, consequent, alternate);
     },
 
     Type_array(_arr, _left, base_type, _right) {
-      return core.arrayType(base_type.rep())
+      return core.arrayType(base_type.rep());
     },
 
     Type_boolean(_boo_keyword) {
-      return core.boolType
+      return core.boolType;
     },
 
     Type_void(_void_keyword) {
-      return core.voidType
+      return core.voidType;
     },
 
     Type_int(_int_keyword) {
-      return core.intType
+      return core.intType;
     },
 
     // Type_string(_str_keyword) {
     //   return core.stringType
     // },
-
-    // Exp_unary(op, exp) {
-    //   return core.unary(op.sourceString, exp.rep())
-    // },
-
-    Exp_ternary(exp1, _questionMark, _rightArrow, exp2, _doubleRightArrow, exp3) {
-      return core.conditional(exp1.rep(), exp2.rep(), exp3.rep())
+    
+    Exp_unary(unaryOp, exp) {
+      const [op, operand] = [unaryOp.sourceString, exp.rep()];
+      let type;
+      if (op === "-") {
+        mustHaveNumericType(operand, { at: exp });
+        type = operand.type;
+      } else if (op === "!") {
+        mustHaveBooleanType(operand, { at: exp });
+        type = BOOLEAN;
+      }
+      return core.unary(op, operand, type);
+    },
+    
+    Exp_ternary(
+      exp1,
+      _questionMark,
+      _rightArrow,
+      exp2,
+      _doubleRightArrow,
+      exp3
+    ) {
+      return core.conditional(exp1.rep(), exp2.rep(), exp3.rep());
     },
 
     Exp1_binary(exp1, op, exp2) {
-      let left = exp1.rep()
-      mustHaveBooleanType(left, { at: exp1})
+      let left = exp1.rep();
+      mustHaveBooleanType(left, { at: exp1 });
       for (let e of exp2.children) {
-        let right = e.rep()
-        mustHaveBooleanType(right, { at: e })
-        left = core.binary(op.sourceString, left, right, BOOLEAN)
+        let right = e.rep();
+        mustHaveBooleanType(right, { at: e });
+        left = core.binary(op.sourceString, left, right, BOOLEAN);
       }
-      return left
+      return left;
     },
 
     Exp2_binary(exp1, op, exp2) {
-      let left = exp1.rep()
-      mustHaveBooleanType(left, { at: exp1})
+      let left = exp1.rep();
+      mustHaveBooleanType(left, { at: exp1 });
       for (let e of exp2.children) {
-        let right = e.rep()
-        mustHaveBooleanType(right, { at: e })
-        left = core.binary(op.sourceString, left, right, BOOLEAN)
+        let right = e.rep();
+        mustHaveBooleanType(right, { at: e });
+        left = core.binary(op.sourceString, left, right, BOOLEAN);
       }
-      return left
+      return left;
     },
 
     Exp3_binary(exp1, relop, exp2) {
-      const [left, op, right] = [exp1.rep(), relop.sourceString, exp2.rep()]
+      const [left, op, right] = [exp1.rep(), relop.sourceString, exp2.rep()];
       if (["<", "<=", ">", ">="].includes(op)) {
-        mustHaveNumericOrStringType(left, { at: exp1 })
+        mustHaveNumericOrStringType(left, { at: exp1 });
       }
-      mustBothHaveTheSameType(left, right, { at: relop })
-      return core.binary(op, left, right, BOOLEAN)
+      mustBothHaveTheSameType(left, right, { at: relop });
+      return core.binary(op, left, right, BOOLEAN);
     },
 
     Exp4_binary(exp1, addOp, exp2) {
-      const [left, op, right] = [exp1.rep(), addOp.sourceString, exp2.rep()]
+      const [left, op, right] = [exp1.rep(), addOp.sourceString, exp2.rep()];
       if (op === "+") {
-        mustHaveNumericOrStringType(left, { at: exp1 })
+        mustHaveNumericOrStringType(left, { at: exp1 });
       } else {
-        mustHaveNumericType(left, { at: exp1 })
+        mustHaveNumericType(left, { at: exp1 });
       }
-      mustBothHaveTheSameType(left, right, { at: addOp })
-      return core.binary(op, left, right, left.type)
+      mustBothHaveTheSameType(left, right, { at: addOp });
+      return core.binary(op, left, right, left.type);
     },
 
     Exp5_binary(exp1, mulOp, exp2) {
-      const [left, op, right] = [exp1.rep(), mulOp.sourceString, exp2.rep()]
-      mustHaveNumericType(left, { at: exp1 })
-      mustBothHaveTheSameType(left, right, { at: mulOp })
-      return core.binary(op, left, right, left.type)
+      const [left, op, right] = [exp1.rep(), mulOp.sourceString, exp2.rep()];
+      mustHaveNumericType(left, { at: exp1 });
+      mustBothHaveTheSameType(left, right, { at: mulOp });
+      return core.binary(op, left, right, left.type);
     },
 
     Exp6_binary(exp1, powerOp, exp2) {
-      const [left, op, right] = [exp1.rep(), powerOp.sourceString, exp2.rep()]
-      mustHaveNumericType(left, { at: exp1 })
-      mustBothHaveTheSameType(left, right, { at: powerOp })
-      return core.binary(op, left, right, left.type)
+      const [left, op, right] = [exp1.rep(), powerOp.sourceString, exp2.rep()];
+      mustHaveNumericType(left, { at: exp1 });
+      mustBothHaveTheSameType(left, right, { at: powerOp });
+      return core.binary(op, left, right, left.type);
     },
 
     Primitive_parens(_open, exp, _close) {
-      return exp.rep()
+      return exp.rep();
     },
 
     Primitive_arrayexp(_open, args, _close) {
-      const elements = args.asIteration().children.map(e => e.rep())
-      mustAllHaveSameType(elements, { at: args })
-      return core.arrayExpression(elements)
+      const elements = args.asIteration().children.map((e) => e.rep());
+      mustAllHaveSameType(elements, { at: args });
+      return core.arrayExpression(elements);
     },
 
     Primitive_id(id, _postfix) {
@@ -483,43 +515,44 @@ export default function analyze(match) {
     Primitive_call(id, open, expList, _close) {
       // ids used in calls must have already been declared and must be
       // bound to function entities, not to variable entities.
-      const callee = context.lookup(id.sourceString)
-      mustHaveBeenFound(callee, id.sourceString, { at: id })
-      mustBeAFunction(callee, { at: id })
-      const exps = expList.asIteration().children
-      const targetTypes = callee.type.paramTypes
-      mustHaveCorrectArgumentCount(exps.length, callee.type.paramCount, { at: open })
+      const callee = context.lookup(id.sourceString);
+      mustHaveBeenFound(callee, id.sourceString, { at: id });
+      mustBeAFunction(callee, { at: id });
+      const exps = expList.asIteration().children;
+      const targetTypes = callee.type.paramTypes;
+      mustHaveCorrectArgumentCount(exps.length, callee.type.paramCount, {
+        at: open,
+      });
       const args = exps.map((exp, i) => {
-        const arg = exp.rep()
-        mustBeAssignable(arg, { toType: targetTypes[i] }, { at: exp })
-        return arg
-      })
-      return core.functionCall(callee, args)
+        const arg = exp.rep();
+        mustBeAssignable(arg, { toType: targetTypes[i] }, { at: exp });
+        return arg;
+      });
+      return core.functionCall(callee, args);
     },
 
     Primitive_void(_) {
-      return core.voidType
+      return core.voidType;
     },
 
     Primitive_true(_) {
-      return true
+      return true;
     },
 
     Primitive_false(_) {
-      return false
+      return false;
     },
 
     num(_whole, _point, _fraction, _e, _sign, _exponent) {
-      return Number(this.sourceString)
+      return Number(this.sourceString);
     },
 
     stringlit(_openQuote, _chars, _closeQuote) {
       // Carlos strings will be represented as plain JS strings, including
       // the quotation marks
-      return this.sourceString
+      return this.sourceString;
     },
-
-  })
+  });
 
   return builder(match).rep()
 }
--- test/generator.test.js
+++ test/generator.test.js
@@ -13,150 +13,19 @@ const fixtures = [
     name: "small",
     source: `
       let Int x = 3 * 7
-      x = x+1
-      x = x-1
+      x = x + 1
+      x = x - 1
       let Boo y = ripe
-      y = 5 ** -x / -100 > - x || rotten
-      Plant((y && y) || rotten || (x*2) != 5)|
+      y = ((5 ** (-x) / (-100)) > (-x))
+      Plant((y && y) || (x*2) != 5)
     `,
     expected: dedent`
-      let x_1 = 21;
-      x_1++;
-      x_1--;
+      let x_1 = (3 * 7);
+      x_1 = (x_1 + 1);
+      x_1 = (x_1 - 1);
       let y_2 = true;
       y_2 = (((5 ** -(x_1)) / -(100)) > -(x_1));
-      console.log(((y_2 && y_2) || ((x_1 * 2) !== 5)));
-    `,
-  },
-  {
-    name: "if",
-    source: `
-      let Int x = 0
-      if (x == 0) : Plant(1)|
-      if (x == 0) : Plant(1)| else: Plant(2)|
-      if (x == 0) : Plant(1)| else if (x==2): Plant(3)|
-      if (x == 0) : Plant(1)| else if (x==2): Plant(3)| else: Plant(4)|
-    `,
-    expected: dedent`
-      let x_1 = 0;
-      if ((x_1 === 0)) {
-        console.log("1");
-      }
-      if ((x_1 === 0)) {
-        console.log(1);
-      } else {
-        console.log(2);
-      }
-      if ((x_1 === 0)) {
-        console.log(1);
-      } else
-        if ((x_1 === 2)) {
-          console.log(3);
-        }
-      if ((x_1 === 0)) {
-        console.log(1);
-      } else
-        if ((x_1 === 2)) {
-          console.log(3);
-        } else {
-          console.log(4);
-        }
-    `,
-  },
-  {
-    name: "while",
-    source: `
-      let Int x = 0
-      while x < 5:
-        let Int y = 0
-        while y < 5:
-          y = y + 1
-          Plant(x * y)|
-        x = x + 1
-      |
-    `,
-    expected: dedent`
-      let x_1 = 0;
-      while ((x_1 < 5)) {
-        let y_2 = 0;
-        while ((y_2 < 5)) {
-          console.log((x_1 * y_2));
-          y_2 = (y_2 + 1);
-          break;
-        }
-        x_1 = (x_1 + 1);
-      }
-    `,
-  },
-  {
-    name: "functions",
-    source: `
-      let Int z = 5
-      pick f( Int x, Boo y) -> Nothing :
-      if x>10:
-	  Plant(x)|
-      serve Nothing|
-      
-      pick g()-> Boo :
-      serve rotten|
-      
-      f(z, g()) 
-    `,
-    expected: dedent`
-      let z_1 = 5;
-      function f_2(x_3, y_4) {
-        if x >10{
-            print(x);
-        }
-        return;
-      }
-      function g_5() {
-        return false;
-      }
-      f_2(z_1, g_5());
-    `,
-  },
-  {
-    name: "arrays",
-    source: `
-      let Bunch (Boo) a = [ripe, rotten, ripe]
-      let Bunch (Int) b = [10, 20, 30]
-      const Bunch c = []
-      const d = random b;
-      print(a[1] || (b[0] < 88 ? rotten : ripe));
-    `,
-    expected: dedent`
-      let a_1 = [true,false,true];
-      let b_2 = [10,(a_1.length - 20),30];
-      let c_3 = [];
-      let d_4 = ((a=>a[~~(Math.random()*a.length)])(b_2));
-      console.log((a_1[1] || (((b_2[0] < 88)) ? (false) : (true))));
-    `,
-  },
-  {
-    name: "for loops",
-    source: `
-      let Bunch(Int) b = (0, 7, 14) 
-      for i in b: 
-      Plant(i)|
-      
-      for i in (0,7,4):
-      for j in (4,5,6):
-      Plant(i+j)|
-      |
-      
-    `,
-    expected: dedent`
-      let b_1 = [0,7,14]
-      for i in b_1{
-        console.log(i);
-      }
-
-      for i in [0,7,4]{
-        for j in [4,5,6]{
-            console.log(i+j);
-        }
-      }
+      console.log(((y_2 && y_2) || ((x_1 * 2) != 5)));
     `,
   },
 ];
